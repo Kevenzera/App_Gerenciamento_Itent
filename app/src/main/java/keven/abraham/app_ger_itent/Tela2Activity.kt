0bed4219f76ec821e45e9617e1cec99d456253c7
Laboratórios - W10 - Fatec Mauá <labs@labtec.local>
2023-08-26 11:15:45 -0300

    Initial commit

---
+++ app/src/main/java/keven/abraham/app_ger_itent/Tela2Activity.kt
@@ -0,0 +1,11 @@
+package keven.abraham.app_ger_itent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela2)
+    }
+}
